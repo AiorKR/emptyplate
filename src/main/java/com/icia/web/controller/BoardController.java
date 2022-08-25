4b3c6453d1273cf129b9a74307202a6260023a9c
AiorKR <106389769+AiorKR@users.noreply.github.com>
2022-08-25 15:59:06 +0900

    Update BoardController.java

--- src/main/java/com/icia/web/controller/BoardController.java
+++ src/main/java/com/icia/web/controller/BoardController.java
@@ -142,11 +142,7 @@ public class BoardController
 		if(!StringUtil.isEmpty(bbsTitle) && !StringUtil.isEmpty(bbsContent))
 		{
 			Board board = new Board();
-<<<<<<< Updated upstream
 			board.setBbsNo(bbsNo);
-=======
-			
->>>>>>> Stashed changes
 			board.setUserUID(cookieUserUID);
 			board.setBbsTitle(bbsTitle);
 			board.setBbsContent(bbsContent);
