507e02ade02dd20d534b7de452ca78e34bbbe244
woqpxkg <woqpxkg1120@kakao.com>
2022-09-17 22:29:21 +0900

    Reservation #12
    -- shopConteoller에서 자리 조회시 인원에 따라 현재 예약인원만큼 자리가 있는지 확인하여 인원보다 자리가 없다면 결제버튼을 잠궈 날짜 또는 시간을 다시 설정하도록 변경
    
    --payController 쿠키를 사용해서 시킨 메뉴, 시킨 수량등 정보 전달 후 삭제 (현제 자리부분 또한 쿠키로 할지, 한번 더 셀렉트하여 예약자리 조회할지 생각하고 있습니다.)

--- src/main/java/com/icia/web/controller/PayController.java
+++ src/main/java/com/icia/web/controller/PayController.java
@@ -21,6 +21,7 @@ import com.icia.web.model.Order;
 import com.icia.web.model.OrderMenu;
 import com.icia.web.model.Response;
 import com.icia.web.model.Shop;
+import com.icia.web.model.ShopReservationTable;
 import com.icia.web.model.Toss;
 import com.icia.web.model.User;
 import com.icia.web.service.PayService;
@@ -36,27 +37,11 @@ public class PayController {
 	//쿠키명 지정
 	@Value("#{env['auth.cookie.name']}")
 	private String AUTH_COOKIE_NAME;
-	
-	//통신할 기본 호스트 주소
-	@Value("#{env['toss.host']}")
-	private String TOSS_HOST;
-	
-	//시크릿키 (카카오페이의 어드민키 처럼 권한이 높음)
-	@Value("#{env['toss.secret.key']}")
-	private String TOSS_SECRET_KEY;
-	
+
 	//브라우저 단에서 사용될 키
 	@Value("#{env['toss.client.key']}")
 	private String TOSS_CLIENT_KEY;
-	
-	//상점 아이디
-	@Value("#{env['toss.mid']}")
-	private String TOSS_MID;
-	
-	//결재승인 URL
-	@Value("#{env['toss.confirm.url']}")
-	private String TOSS_CONFIRM_URL;
-		
+
 	//결제 성공  URL
 	@Value("#{env['toss.success.url']}")
 	private String TOSS_SUCCESS_URL;
@@ -89,40 +74,42 @@ public class PayController {
 		order.setShopUID(HttpUtil.get(request, "shopUID")); //shopUID
 		String reservationDate = HttpUtil.get(request, "reservationDate"); //예약일
 		String reservationTime = HttpUtil.get(request, "reservationTime"); //예약시간
-		String reservationPeople = HttpUtil.get(request, "reservationPeople"); //예약인원
+		int reservationPeople = Integer.parseInt((HttpUtil.get(request, "reservationPeople"))); //예약인원
+		ShopReservationTable shopReservationTable = new ShopReservationTable();
+		
+		logger.debug("parsInt : " + Integer.parseInt(reservationDate));
+		
+		
 		ArrayList<String> requstOrderMenuList = new ArrayList<String>(); //jsp에서 주문목록 받아올때 사용할 arrayList
 		Toss toss = new Toss();
+		
 		order.setTotalAmount(HttpUtil.get(request, "totalAmount", 0));
-		
-		
-		logger.debug("shopUID : " + order.getShopUID());
-		logger.debug("reservationDate : " + reservationDate);
-		logger.debug("reservationTime : " + reservationTime);
-		logger.debug("reservationPeople : " + reservationPeople);
+		order.setReservationPeople(reservationPeople);
 		order.setShopUID(order.getShopUID());
 		
 		toss.setTossClientKey(TOSS_CLIENT_KEY);
-		toss.setTossSecretKey(TOSS_SECRET_KEY);
-		toss.setTossMid(TOSS_MID);
-		toss.setTossConfrimUrl(TOSS_CONFIRM_URL);
 		toss.setTossSuccessUrl(TOSS_SUCCESS_URL);
 		toss.setTossCancelUrl(TOSS_CANCEL_URL);
 		toss.setTossFailUrl(TOSS_FAIL_URL);
-	
-		
+
 		order.setToss(toss);
 		
 		if(!StringUtil.isEmpty(cookieUserUID)) {
 			User user = userService.userUIDSelect(cookieUserUID);
 			if(!StringUtil.isEmpty(order.getShopUID()) && user != null) {
-				order.setUserName(user.getUserName());
+				order.setReservationName(user.getUserName());
+				order.setReservationPhon(user.getUserPhone());
 				order.setUserUID(user.getUserUID());
+				shopReservationTable.setShopReservationDate(reservationDate);
+				shopReservationTable.setShopReservationTime(reservationTime);
+			
+				order.setShopReservationTable(shopReservationTable);
+				
 				Shop shop = shopService.shopViewSelect(order.getShopUID());
 				if(shop != null) {
 					for(int i=0; i < shop.getShopMenu().size(); i++) {
 						String requestName = "orderMenu" + i;
 						requstOrderMenuList.add(HttpUtil.get(request, requestName));
-						
 						logger.debug("orderMenuList[" + i + "]" + requstOrderMenuList.get(i));
 		
 						String[] split = requstOrderMenuList.get(i).split(","); // 구분자","로 잘라서 배열에 담음
@@ -132,23 +119,44 @@ public class PayController {
 						orderMenu.setOrderMenuName(split[0]);
 						orderMenu.setOrderMenuQuantity(Integer.parseInt(split[1]));
 						
+						if(StringUtil.equals(orderMenu.getOrderMenuName(), shop.getShopMenu().get(i).getShopMenuName())) { //메뉴 이름과 같은 메뉴이름을 찾아서 가격 세팅
+							orderMenu.setOrderMenuPrice(shop.getShopMenu().get(i).getShopMenuPrice());
+						}
+						
 						if(orderMenu.getOrderMenuQuantity() > 0) {
 							orderMenuList.add(orderMenu);
 						}
 					}
 					
 					order.setOrderMenu(orderMenuList);
+					order.setShopName(shop.getShopName());
 					
 					if(order.getOrderMenu().size() > 0 && order.getTotalAmount() > 0) {
-						long count = shopService.orderUIDcreate();
+						long count = shopService.orderUIDcreate(); //count는 orderuid에 사용될 seq값을 가져옴
 						if(count != 0) {
 							order.setOrderUID(shop.getShopUID() + "_" +count);
+							CookieUtil.addCookie(response, "/", -1, "reservationDate", CookieUtil.stringToHex(order.getShopReservationTable().getShopReservationDate())); //예약일 쿠키 추가
+							CookieUtil.addCookie(response, "/", -1, "reservationTime", CookieUtil.stringToHex(order.getShopReservationTable().getShopReservationDate())); //예약시간 쿠키 추가
+							CookieUtil.addCookie(response, "/", -1, "reservationPeople", CookieUtil.stringToHex(Integer.toString(order.getReservationPeople()))); //예약인원
+							CookieUtil.addCookie(response, "/", -1, "reservationTotalAmount", CookieUtil.stringToHex(Integer.toString(order.getTotalAmount()))); //총 금액
+							CookieUtil.addCookie(response, "/", -1, "shopUID", CookieUtil.stringToHex(order.getShopUID())); //매장 UID
+								
+							CookieUtil.addCookie(response, "/", -1, "orderMenuSize", CookieUtil.stringToHex(Integer.toString(order.getOrderMenu().size())));
+							for(int i=0; i < order.getOrderMenu().size(); i++) {
+								String cookieOrderMenuName = "orderMenuName" + i; //동적으로 쿠키 이름 생성
+								String cookieOrderMenuPrice = "orderMenuPrice" + i;
+								String cookieOrderMenuQuantity = "orderMenuQuantity" + i;
+								
+								CookieUtil.addCookie(response, "/", -1, cookieOrderMenuName, CookieUtil.stringToHex(order.getOrderMenu().get(i).getOrderMenuName()));
+								CookieUtil.addCookie(response, "/", -1, cookieOrderMenuPrice, CookieUtil.stringToHex(Integer.toString(order.getOrderMenu().get(i).getOrderMenuPrice())));
+								CookieUtil.addCookie(response, "/", -1, cookieOrderMenuQuantity, CookieUtil.stringToHex(Integer.toString(order.getOrderMenu().get(i).getOrderMenuQuantity())));
+							}
+							
 							ajax.setResponse(0, "success", order);
 						}
 						else {
 							ajax.setResponse(400, "badRequest");
 						}
-						
 					}
 					else {
 						ajax.setResponse(400, "menu is empty");
@@ -167,28 +175,65 @@ public class PayController {
 	}
 	@RequestMapping(value="/pay/paySuccess", method=RequestMethod.GET)
 	public String tossPayReady(HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("페이 성공 들어옴");
-		
 		String orderId = HttpUtil.get(request, "orderId");
 		String paymentKey = HttpUtil.get(request, "paymentKey");
 		String amount = HttpUtil.get(request, "amount");
-		
-		Order order = new Order();
+		String url = "";
+		Order RequestOrder = new Order(); //값 세팅할 order
+		Order order = null;
 		Toss toss = new Toss();
+		String shopUID = CookieUtil.getHexValue(request, "shopUID");
 		
-		toss.setPaymentKey(paymentKey);
-		order.setToss(toss);
-		order.setOrderUID(orderId);
-		order.setTotalAmount(Integer.parseInt(amount));
+		List<OrderMenu> orderMenuList = new ArrayList<OrderMenu>();
+		OrderMenu orderMenu = new OrderMenu();
 		
-		logger.debug("orderId : " + orderId);
-		logger.debug("paymentKey : " + paymentKey);
-		logger.debug("amount : " + amount);
+		int orderMenuSize = Integer.parseInt(CookieUtil.getHexValue(request, "orderMenuSize"));
+		CookieUtil.deleteCookie(request, response, "/", "orderMenuSize");
 		
-		payService.toss(order);
+		String totalAmount = CookieUtil.getHexValue(request, "reservationTotalAmount");
+		if(StringUtil.equals(totalAmount, amount)) { //totalAmount는 쿠키로 받은 값 (토스에 보낸 값), amount는 parameter로 받은 값 (토스에서 보내준 값)
+			int reservtionPeple = Integer.parseInt(CookieUtil.getHexValue(request, "reservationPeople"));
+			String reservationDate = CookieUtil.getHexValue(request, "reservationDate");
+			String reservationTime = CookieUtil.getHexValue(request, "reservationTime");
+			
+			
+			for(int i=0; i < orderMenuSize; i++) {
+				String cookieOrderMenuName = "orderMenuName" + i; //동적으로 쿠키 이름 생성
+				String cookieOrderMenuPrice = "orderMenuPrice" + i;
+				String cookieOrderMenuQuantity = "orderMenuQuantity" + i;
+				
+				orderMenu.setOrderMenuName(CookieUtil.getHexValue(request, cookieOrderMenuName));
+				orderMenu.setOrderMenuPrice(Integer.parseInt(CookieUtil.getHexValue(request, cookieOrderMenuPrice)));
+				orderMenu.setOrderMenuQuantity(Integer.parseInt(CookieUtil.getHexValue(request, cookieOrderMenuQuantity)));
+				orderMenuList.add(orderMenu);
+				
+				CookieUtil.deleteCookie(request, response, "/", cookieOrderMenuName);
+				CookieUtil.deleteCookie(request, response, "/", cookieOrderMenuPrice);
+				CookieUtil.deleteCookie(request, response, "/", cookieOrderMenuQuantity);
+			}
+			
+			
+			toss.setPaymentKey(paymentKey);
+			RequestOrder.setToss(toss);
+			RequestOrder.setOrderUID(orderId);
+			RequestOrder.setTotalAmount(Integer.parseInt(amount));
+			RequestOrder.setOrderMenu(orderMenuList);
+						
+			order = payService.toss(RequestOrder);
+			Shop shop = shopService.shopViewSelect(shopUID);
+			shop.setReservationDate(order.getShopReservationTable().getShopReservationDate());
+			shop.setReservationTime(order.getShopReservationTable().getShopReservationTime());
+			shop.setShopTotalTable(shopService.shopReservationCheck(shop));
+			
+			
+		}
+		if( order != null) {
+			url = "/pay/paySuccess"; 
+		}
+		else {
+			url = "/pay/payFail"; //order객체가 null이라면 결제 실패페이지로 이동
+		}
 		
-		
-		
-		return "/reservation/list";
+		return url;
 	}
 }
--- src/main/java/com/icia/web/service/PayService.java
+++ src/main/java/com/icia/web/service/PayService.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 import java.util.Base64;
 import java.util.HashMap;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,10 +12,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import com.icia.common.util.StringUtil;
+import com.icia.web.model.Board;
+import com.icia.web.model.BoardFile;
 import com.icia.web.model.Order;
 import com.icia.web.model.Toss;
 
@@ -33,34 +36,13 @@ public class PayService {
 	@Value("#{env['toss.secret.key']}")
 	private String TOSS_SECRET_KEY;
 	
-	//브라우저 단에서 사용될 키
-	@Value("#{env['toss.client.key']}")
-	private String TOSS_CLIENT_KEY;
-	
-	//상점 아이디
-	@Value("#{env['toss.mid']}")
-	private String TOSS_MID;
-	
 	//결재승인 URL
 	@Value("#{env['toss.confirm.url']}")
 	private String TOSS_CONFIRM_URL;
-		
-	//결제 성공  URL
-	@Value("#{env['toss.success.url']}")
-	private String TOSS_SUCCESS_URL;
 	
-	//결재 취소 URL
-	@Value("#{env['toss.cancel.url']}")
-	private String TOSS_CANCEL_URL;
-
-	
-	//결재 실패 URL
-	@Value("#{env['toss.fail.url']}")
-	private String TOSS_FAIL_URL;
-
-	
-	public void toss(Order order) {      
-		if(order != null)
+	public Order toss(Order requestOrder) {   
+		Order order = null;
+		if(requestOrder != null)
 	      {
 	         RestTemplate restTemplate = new RestTemplate();
 	         restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
@@ -81,20 +63,32 @@ public class PayService {
 	         //서버로 요청할 body
 	         HashMap<String,String> params = new HashMap<String, String>();
 	         
-	         params.put("amount", Integer.toString(order.getTotalAmount()));
-	         params.put("orderId", order.getOrderUID());
-	         params.put("paymentKey", order.getToss().getPaymentKey());
+	         params.put("amount", Integer.toString(requestOrder.getTotalAmount()));
+	         params.put("orderId", requestOrder.getOrderUID());
+	         params.put("paymentKey", requestOrder.getToss().getPaymentKey());
 	         
 	         HttpEntity<HashMap<String,String>> body = new HttpEntity<HashMap<String,String>>(params, headers);
 	         
 	         try
 	         {
-	        	 HashMap response = restTemplate.postForObject(new URI(TOSS_HOST + TOSS_CONFIRM_URL), body, HashMap.class);
-	            logger.debug(" response : " + response);
-	           
+	        	 @SuppressWarnings("unchecked")
+				HashMap<String,String> response = restTemplate.postForObject(new URI(TOSS_HOST + TOSS_CONFIRM_URL), body, HashMap.class);
+	
 	            if(response != null)
 	            {
-	               logger.debug("[PayService]toss : " + response);
+	            	logger.debug("결과 : " + response);
+	               order = new Order();
+	               if(StringUtil.equals(response.get("status"), "DONE")) { //토스에서 결제가 완료 되었는가  (DONE - 결제 완료됨)
+		               order.setOrderStatus("Y");
+			               order.setOrderUID(response.get("orderId")); //예약번호
+			               order.setOrderRegDate(response.get("approvedAt").substring(0, 10).replace("-", "")); //토스에서 결제 완료 된 시간 (yyyy-mm-dd 까지 남기고 자른 후 -를 "" 바꿈)
+			               order.getToss().setPaymentKey(requestOrder.getToss().getPaymentKey()); //토스에서 부여해주는 결제 건에 대한 고유한 키 값, 최대 길이 200
+			               order.setTotalAmount((requestOrder.getTotalAmount())); // 총합 금액
+
+	               }
+	               else {
+	            	   order = null;
+	               }
 	            }
 
 	         }
@@ -111,6 +105,29 @@ public class PayService {
 	      {
 	         logger.error("[PayService]toss order is null");
 	      }
+		return order;
 
 	}
+	
+	
+//	@Transactional(propagation=Propagation.REQUIRED, rollbackFor=Exception.class) //최종결제 및 DB에 정보 INSERT
+//	public int payResult(Order Order) throws Exception
+//	{
+//		long 
+//		
+//		int count = boardDao.boardInsert(board);
+//		
+//		//첨부파일 등록
+//		if(count > 0 && board.getBoardFile() != null)
+//		{	
+//			BoardFile boardFile = board.getBoardFile();
+//			boardFile.setBbsSeq(board.getBbsSeq());
+//			boardFile.setFileOrgName(Long.toString(bbsSeq) + "."+ boardFile.getFileExt());
+//			boardFile.setFileSeq((short)1);
+//			
+//			boardDao.boardFileInsert(board.getBoardFile());
+//		}
+//		
+//		return count;
+//	}
 }
